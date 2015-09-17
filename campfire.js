b6318ee896a7428591aa3f18049b5a288827da9f
Erica Irving <contact.eirving@gmail.com>
2015-09-17 19:51:01 -0400

    add story archive feature

--- campfire.js
+++ campfire.js
@@ -6,18 +6,20 @@ if (Meteor.isClient) {
   Meteor.startup(function() {
     //Dynamic, responsive background image
     $.backstretch("dark_wood.png");
-    //Start countdown timer for three minutes
-    number = 10;
+    //Open a new story
     Meteor.call("createNewStory");
+    //Start countdown timer for three minutes
+    number = 180;
   });
 
   Meteor.setInterval(function() {
     //Reset timer and clear all words when countdown hits 0
     if(number === 0) {
-      Meteor.call("removeAllWords");
-      number = 10;
+      Meteor.call("clearAllWords");
+      Meteor.call("createNewStory");
+      number = 180;
     }
-    //Decrement by one every second
+    //Otherwise, decrement by one every second
     else {
       number--;
     }
@@ -47,8 +49,20 @@ if (Meteor.isClient) {
     },
     seconds: function () {
       return Session.get("seconds");
+    },
+    stories: function () {
+      return Stories.find({});
     }
 
+  });
+
+  Template.firstLine.helpers({
+   //For each story, return the text of the first ten words, followed by an ellipsis
+   hasContent: function (story) {
+      if (this.storyText.length > 0) {
+        return true;
+      }
+   }
   });
 
   Template.body.events({
@@ -65,32 +79,55 @@ if (Meteor.isClient) {
       // Insert a word into the collection when spacebar is pressed -- word.length condition guards against acccidental submission if spacebar is hit before word
       if (event.keyCode === 32 && word.length > 1) {
         //Add text to the story
-        Words.insert({
-          text: word,
-          createdAt: new Date()
-        });
+        Meteor.call("createNewWord", word);
+        Meteor.call("updateStory");
         // Clear form
         event.target.value = "";
       }
+    },
+
+    "click .lightbox-trigger": function (event) {
+      event.preventDefault();
+      $("#lightbox").css("display", "block");
+    },
+
+    "click .lightbox-close": function () {
+      $("#lightbox").css("display", "none");
     }
 
   });
 
 }
 
+Meteor.methods({
+  createNewWord: function (word) {
+    Words.insert({
+      text: word,
+      createdAt: new Date()
+    });
+  },
+  clearAllWords: function() {
+    return Words.remove({});
+  },
+  createNewStory: function () {
+    Stories.insert({
+      storyText: [],
+      createdAt: new Date()
+    });
+  },
+  updateStory: function () {
+    var currentStory = Stories.findOne({}, {sort: {createdAt: -1}});
+    var latestWord = Words.findOne({}, {sort: {createdAt: -1}});
+    Stories.update(
+      {_id: currentStory._id},
+      {$push: {storyText: latestWord}}
+    );
+  }
+});
+
 if (Meteor.isServer) {
 
   Meteor.startup(function() {
-    return Meteor.methods({
-      removeAllWords: function() {
-        return Words.remove({});
-      },
-      createNewStory: function () {
-        Stories.insert({
-          createdAt: new Date ()
-        });
-      }
-    });
 
   });
 }
