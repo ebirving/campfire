8519c95d292ca050dfdd7fcec32fa46cae2550ba
Erica Irving <contact.eirving@gmail.com>
2015-09-18 16:32:04 -0400

    terrible commit message

--- campfire.js
+++ campfire.js
@@ -9,32 +9,9 @@ if (Meteor.isClient) {
     //Open a new story
     Meteor.call("createNewStory");
     //Start countdown timer for three minutes
-    number = 180;
+    counter = 0;
+    console.log(counter);
   });
-
-  Meteor.setInterval(function() {
-    //Reset timer and clear all words when countdown hits 0
-    if(number === 0) {
-      Meteor.call("clearAllWords");
-      Meteor.call("createNewStory");
-      number = 180;
-    }
-    //Otherwise, decrement by one every second
-    else {
-      number--;
-    }
-    //Pass countdown value to body template helper, converted to min and sec
-    var minutes = Math.floor(number/60);
-    Session.set("minutes", minutes);
-
-    var seconds = (number % 60);
-    if (seconds < 10) {
-      Session.set("seconds", "0" + seconds);
-    }
-    else {
-      Session.set("seconds", seconds);
-    }
-  }, 1000);
 
   Template.body.helpers({
 
@@ -44,28 +21,75 @@ if (Meteor.isClient) {
     wordCount: function () {
       return Words.find({}).count({});
     },
+    stories: function () {
+      return Stories.find({});
+    }
+  });
+
+  Template.timer.helpers({
     minutes: function () {
       return Session.get("minutes");
     },
     seconds: function () {
       return Session.get("seconds");
-    },
-    stories: function () {
-      return Stories.find({});
     }
-
   });
 
-  Template.firstLine.helpers({
-   //For each story, return the text of the first ten words, followed by an ellipsis
-   hasContent: function (story) {
-      if (this.storyText.length > 0) {
+  Template.timer.events({
+
+    "click, .start-button": function (event) {
+      event.preventDefault();
+      counter++;
+      console.log(counter);
+      if (counter === 1) {
+        number = 180;
+        Meteor.call("clearAllWords");
+        Meteor.call("createNewStory");
+        Meteor.setInterval(function() {
+          //Reset timer and clear all words when countdown hits 0
+          if(number === 0) {
+            Meteor.call("clearAllWords");
+            counter = 0;
+            return true;
+          }
+          //Otherwise, decrement by one every second
+          else {
+            number--;
+          }
+          //Pass countdown value to body template helper, converted to min and sec
+          var minutes = Math.floor(number/60);
+          Session.set("minutes", minutes);
+
+          var seconds = (number % 60);
+          if (seconds < 10) {
+            Session.set("seconds", "0" + seconds);
+          }
+          else {
+            Session.set("seconds", seconds);
+          }
+        }, 1000);
+      }
+    }
+  });
+
+  Template.story.helpers({
+    //Only render story in archive if it has storyText
+    hasContent: function () {
+      if(this.storyText.length > 0) {
         return true;
       }
-   }
+    }
+  });
+
+  Template.story.events({
+    "click .delete": function () {
+      Stories.remove(this._id);
+    }
   });
 
   Template.body.events({
+
+
 
     "submit .new-word": function(event){
       // Prevent default browser form submit
@@ -76,6 +100,7 @@ if (Meteor.isClient) {
       event.preventDefault();
       // Get value from form element
       var word = event.target.value;
+      var typing = $("input[name='word']").val();
       // Insert a word into the collection when spacebar is pressed -- word.length condition guards against acccidental submission if spacebar is hit before word
       if (event.keyCode === 32 && word.length > 1) {
         //Add text to the story
@@ -83,6 +108,10 @@ if (Meteor.isClient) {
         Meteor.call("updateStory");
         // Clear form
         event.target.value = "";
+        $("#wordInProgress").empty();
+      }
+      else {
+        $("#wordInProgress").text(typing);
       }
     },
 
@@ -97,37 +126,52 @@ if (Meteor.isClient) {
 
   });
 
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 Meteor.methods({
+
   createNewWord: function (word) {
     Words.insert({
       text: word,
-      createdAt: new Date()
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
   },
+
   clearAllWords: function() {
     return Words.remove({});
   },
+
   createNewStory: function () {
     Stories.insert({
       storyText: [],
+      storyAuthors: [],
       createdAt: new Date()
     });
   },
-  updateStory: function () {
-    var currentStory = Stories.findOne({}, {sort: {createdAt: -1}});
-    var latestWord = Words.findOne({}, {sort: {createdAt: -1}});
+
+  updateStory: function() {
+    var currentStory = Stories.find({}, {sort: {createdAt: -1}, limit: 1 }).fetch();
+    console.log(currentStory[0].storyText);
+    var latestWord = Words.find({}, {sort: {createdAt: -1}, limit: 1 }).fetch();
+    console.log(latestWord[0].text);
+    console.log(latestWord[0].username);
     Stories.update(
-      {_id: currentStory._id},
-      {$push: {storyText: latestWord}}
+      {_id: currentStory[0]._id},
+      {$push: {storyText: latestWord[0].text}, $addToSet: {storyAuthors: latestWord[0].username}}
     );
-  }
+  },
+
 });
 
-if (Meteor.isServer) {
-
-  Meteor.startup(function() {
-
-  });
-}
+// if (Meteor.isServer) {
+//
+//   Meteor.startup(function() {
+//
+//   });
+// }
